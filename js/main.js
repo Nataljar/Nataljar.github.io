546d37a11ced1cd98fb6c073c4a1e7325bad430f
Nataljar <natalja.rau@gmx.de>
2023-10-29 12:53:57 +0100

    First Commit

---
+++ js/main.js
@@ -0,0 +1,152 @@
+
+/*********************  HERO SECTION **************************/ 
+
+// Background Images changes every 5 seconds
+function changeBg() {
+  const bgImages = [
+    'url("images/header_1.jpg")',
+    'url("images/header_2.jpg")',
+    'url("images/header_3.jpg")',
+  ]
+
+  const header = document.querySelector(".header-image-container");
+  const bg = bgImages[Math.floor(Math.random()* bgImages.length)];
+  header.style.backgroundImage = bg;
+}
+
+setInterval(changeBg, 5000);
+
+/*********************  GALERIE  **************************/ 
+
+// Scroll to Top - Gallery
+
+const goTopBtn = document.querySelector('.to-top');
+
+window.addEventListener('scroll', checkHeight)
+
+function checkHeight(){
+  if(window.scrollY > 700) {
+    goTopBtn.style.display = "flex"
+  } else {
+    goTopBtn.style.display = "none"
+  }
+}
+
+if (goTopBtn) {
+goTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+})
+}
+
+
+
+/*********************  Kontaktformular  **************************/ 
+
+// Validierung der Eingabefelder (Quelle: https://www.youtube.com/watch?v=CYlNJpltjMM&ab_channel=JavaScriptAcademy)
+
+const form = document.getElementById("form");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const messageInput = document.getElementById("message");
+
+const successMessage = document.querySelector(".success-message");
+const submitButton = document.getElementById("submit");
+ 
+form.addEventListener("submit", event => {
+
+  event.preventDefault();             // Funktion um das Abesenden und Neuladen der Seite zu verhindern
+
+  validateInputs();               // Funktion um die Eingabefelder zu validieren
+
+  if (formIsValid()) {            // Bedingung, wenn das Formular korrekt ist, wird dieses abgeschickt und neugeladen
+   /* form.submit(); */
+  
+    successMessage.style.display="block";
+    submitButton.click(); // THIS NEEDS TO BE CHECKED IF REALLY CORRECT - FOR NOW NO ERROR MESSAGES!!
+   
+  }  
+});
+
+
+function formIsValid() {        // Funktion, um zu prüfen, ob das Formular korrekt befüllt ist
+  const errorInput = form.querySelector(".input-control.error");
+  return !errorInput;
+}
+
+
+const setError = (element, message) => {                        // Funktion, um einen Fehlerzustand für ein Eingabefeld zu setzen ("element" sthet für das Eingabefeld und "message" enthält die Fehlermeldung)
+  const inputControl = element.parentElement;                   // hier wird das übergeordnete Element des Eingabefelds abgerufen und der Variablen inputControl zugewiesen. Das übergeordnete Element ist der Container, der das Eingabefeld und die Fehlermeldung umgibt.
+  const errorDisplay = inputControl.querySelector(".error");    // hier wird die Fehlermeldung im HTML-Element mit der Klasse "error" gesucht, das sich innerhalb des inputControl-Elements befindet. Die Funktion querySelector sucht das erste Element, das zur angegebenen CSS-Klasse passt, und gibt es zurück. Das gefundene Element wird der Variablen errorDisplay zugewiesen.
+
+  errorDisplay.innerText = message;
+  inputControl.classList.add("error");
+  inputControl.classList.remove("success");
+}
+
+const setSuccess = element => {                                 // Funktion, um einen Fehlerzustand für ein Eingabefeld zu setzen
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerText = "";
+  inputControl.classList.add("success");
+  inputControl.classList.remove("error");
+}
+
+const isValidEmail = email => {                                  // Funktion prüft, ob E-Mail Adresse gültig ist 
+  let regEx =/^[^@\s]+@[^@\s]+\.[^@\s]+$/;
+  return regEx.test(String(email).toLowerCase());               // Die Funktion gibt true zurück, wenn die E-Mail Adresse gültig ist, andernfalls false.
+}
+
+const validateInputs = () => {       // Funktion um die Eingabefelder zu validieren
+const nameValue = nameInput.value.trim();                       // Zuerst werden die Werte der Eingabefelder abgerufen und Leerzeichen am Anfang und Ende entfernt
+const emailValue = emailInput.value.trim();
+const messageValue = messageInput.value.trim();
+
+if (nameValue === "") {
+  setError(nameInput, "Gib deinen vollständigen Namen ein");
+} else {
+  setSuccess(nameInput);
+}
+
+if (emailValue === "") {
+  setError(emailInput, "Gib deine E-Mail Adresse ein");
+} else if (!isValidEmail(emailValue)) {
+  setError(emailInput, "Gib eine gültige E-Mail Adresse ein");
+} else {
+  setSuccess(emailInput);
+}
+
+if (messageValue === "") {
+  setError(messageInput, "Verfasse eine Nachricht");
+} else {
+  setSuccess(messageInput);
+}
+
+// Sobald in das Eingabefeld geklickt wird, wird die Fehlermeldung entfernt
+nameInput.addEventListener("focus", () => {
+  removeError(nameInput);
+});
+
+emailInput.addEventListener("focus", () => {
+  removeError(emailInput);
+});
+
+messageInput.addEventListener("focus", () => {
+  removeError(messageInput);
+});
+
+function removeError(inputElement) {
+  const inputControl = inputElement.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerText = "";
+  inputControl.classList.remove("error");
+}
+
+};
+
+
+
---
+++ js/popup_frankreich.js
@@ -0,0 +1,43 @@
+// Image popup + slider für Frankreich 
+
+const images = [...document.querySelectorAll('.image')];      // es wird nach class="image" gesucht und in ein Array verpackt
+
+const imagePopup = document.querySelector('.image-popup');
+const closeBtn = document.querySelector('.close-btn');
+const largeImage = document.querySelector('.large-image');
+const leftArrow = document.querySelector('.left-arrow');
+const rightArrow = document.querySelector('.right-arrow');
+const body = document.body;
+
+let index = 0; // will track our current image;
+
+images.forEach((item, i) => {                           // Eine Schleife wird verwendet, um jedem Element der Variable images einen Klickereignislistener hinzuzufügen.
+  item.addEventListener('click', () => {
+    updateImage(i);                                     // Funktion wird beim klicken auf ein Bild aufgerufen und der Bildindex an die Funktion übergeben
+    imagePopup.classList.toggle('active');              // durch das Hinzufügen von active wird das popup Fenster sichtbar
+    body.classList.toggle("body-no-scroll"); /* this class is added so that the scrolling of the html page isn't working */
+})
+})
+
+const updateImage = (i) => {
+  let path = `images/galerie_popup_frankreich/img${i + 1}.jpg`;                     // Der path wird mit dem entsprechenden Bildpfad generiert und dem largeImage-Element zugewiesen.
+  largeImage.src = path;
+  index = i;                                            // Der index wird auf den übergebenen Index aktualisiert
+}
+
+closeBtn.addEventListener('click', () => {              // Ein Klickereignislistener wird zum Schließen-Schaltflächen-Element hinzugefügt. Wenn darauf geklickt wird, wird das Bild-Popup durch das Hinzufügen oder Entfernen der CSS-Klasse 'active' ausgeblendet.
+  imagePopup.classList.toggle('active');
+  body.classList.toggle("body-no-scroll");
+})
+
+leftArrow.addEventListener('click', () => {             //Ein Klickereignislistener wird zum Linkspfeil-Element hinzugefügt. Wenn darauf geklickt wird und der aktuelle Index größer als 0 ist, wird das vorherige Bild durch Aufrufen der updateImage-Funktion mit dem vorherigen Index aktualisiert.
+  if (index > 0) {
+    updateImage(index - 1);
+  }
+})
+
+rightArrow.addEventListener('click', () => {
+  if (index < images.length - 1) {
+    updateImage(index + 1);
+  }
+})
